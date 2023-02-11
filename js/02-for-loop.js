d9b63ca9841167b067547d6f528346c4cd5a3428
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-02-11 13:16:52 +0200

    add tasks for

--- js/02-for-loop.js
+++ js/02-for-loop.js
@@ -29,6 +29,20 @@
  * Зарплата каждого работника это случайное число от 500 до 5000
  * Записать сумму в переменную totalSalary и вывести в консоль
  */
+// const min = 500;
+// const max = 5000
+// const employees = 5;
+// let totalSalary = 0;
+
+// for (let i = 1; i <= employees; i += 1) {
+//     console.log(i);
+//     const salery = Math.round(Math.random() * (max - min) + min);
+//     console.log(salery);
+//     totalSalary += salery;
+//     // console.log('Total',totalSalary)
+// }
+
+//     console.log('Total',totalSalary)
 
 
 
@@ -38,3 +52,23 @@
  * Например, если min=0 и max=5, то диапазон 0-5, и в нём два чётных числа - 2 и 4, их сумма 6.
  */
 
+const min = 0;
+const max = 5;
+let total = 0;
+
+for (let i = min; i <= max; i += 1) {
+
+  if (i % 2 !== 0) {
+    // console.log('Не чётное: ', i);
+    continue;
+  }
+
+  // пишем в сумму
+  console.log('чётное: ', i);
+  total += i;
+
+  // аналог +=
+  // total = total + i;
+}
+
+console.log('total: ', total);
